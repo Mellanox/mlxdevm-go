68ef7449b737b77785b02e954edd343a2f638c99
Parav Pandit <parav@nvidia.com>
2021-05-10 14:09:15 +0300

    Initial implementation
    Signed-off-by: Parav Pandit <parav@nvidia.com>

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # mlxdevm-go
 mlxdevm library for for device management in go language
+
+## Overview
+Subfunction(SF) aka scalable function are managed by devlink interface in upstream kernel.
+They can be also managed in Mellanox OFED distrubtion using a similar interface called mlxdevm.
+This is helpful to use SFs in older kernel distributions where devlink interface of latest and greast kernel is not unavailable.
+
+Only difference between the two interfaces are their socket name.
+For example devlink socket name is "devlink" vs mlxdevm socket name is "mlxdevm".
+
+Container orchestration software such as CNI or device plugin which needs to operate over both the interfaces (upstream devlink and mlxdevm) in simple and elegant way.
+
+This package enables orchestration software to use upstream devlink compliant APIs over mlxdevm interface.
+
+## How to use this library in the application?
+
+$ go get github.com/Mellanox/mlxdevm-go
+
+Sample application:
+
+```go
+package main
+
+import (
+    mlxdevm "github.com/Mellanox/mlxdevm-go"
+    "fmt"
+)
+
+func main() {
+    var portAttr mlxdevm.DevLinkPortAddAttrs
+    
+    portAttr.PfNumber = 0
+    
+    portAttr.SfNumber = 88 // Any number starting 0 to 999
+    // To use upstream devlink interface
+    dl_port, err := mlxdevm.DevLinkPortAdd("devlink", "pci", "0000:06:00.0", mlxdevm.DEVLINK_PORT_FLAVOUR_PCI_SF, portAttr)
+    if err != nil {
+        return
+    }
+    fmt.Printf("Port = ", dl_port)
+    
+    portAttr.SfNumber = 99 // Any number starting 0 to 999
+    // To use mlxdevm interface
+    dl_port2, err2 := mlxdevm.DevLinkPortAdd("mlxdevm", "pci", "0000:06:00.0", mlxdevm.DEVLINK_PORT_FLAVOUR_PCI_SF, portAttr)
+    if err2 != nil {
+        return
+    }
+    fmt.Printf("Port = ", dl_port2)
+}
+```
---
+++ devlink_test.go
@@ -0,0 +1,151 @@
+// +build linux
+
+package mlxdevm
+
+import (
+	"errors"
+	"flag"
+	"net"
+	"testing"
+)
+
+func validateArgs(t *testing.T) error {
+	if socket == "" {
+		t.Log("user must specify socket name as devlink or mlxdevm")
+		return errors.New("empty socket name")
+	}
+	if socket != "mlxdevm" && socket != "devlink" {
+		t.Log("user must specify socket name as devlink or mlxdevm")
+		return errors.New("empty socket name")
+	}
+	if bus == "" || device == "" {
+		t.Log("devlink bus and device are empty, skipping test")
+		return errors.New("empty socket name")
+	}
+	return nil
+}
+
+func TestDevLinkGetDeviceList(t *testing.T) {
+	_, err := DevLinkGetDeviceList(socket)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDevLinkGetDeviceByName(t *testing.T) {
+	err := validateArgs(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = DevLinkGetDeviceByName(socket, bus, device)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestDevLinkGetAllPortList(t *testing.T) {
+	ports, err := DevLinkGetAllPortList()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("devlink port count = ", len(ports))
+	for _, port := range ports {
+		t.Log(*port)
+	}
+}
+
+func TestDevLinkAddDelSfPort(t *testing.T) {
+	var addAttrs DevLinkPortAddAttrs
+	err := validateArgs(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+	dev, err := DevLinkGetDeviceByName(socket, bus, device)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	addAttrs.SfNumberValid = true
+	addAttrs.SfNumber = uint32(sfnum)
+	addAttrs.PfNumber = 0
+	port, err2 := DevLinkPortAdd(socket, dev.BusName, dev.DeviceName, 7, addAttrs)
+	if err2 != nil {
+		t.Fatal(err2)
+		return
+	}
+	t.Log(*port)
+	if port.Fn != nil {
+		t.Log("function attributes = ", *port.Fn)
+	}
+	err2 = DevLinkPortDel(socket, dev.BusName, dev.DeviceName, port.PortIndex)
+	if err2 != nil {
+		t.Fatal(err2)
+	}
+}
+
+func TestDevLinkSfPortFnSet(t *testing.T) {
+	var addAttrs DevLinkPortAddAttrs
+	var stateAttr DevlinkPortFnSetAttrs
+
+	err := validateArgs(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+	dev, err := DevLinkGetDeviceByName(socket, bus, device)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	addAttrs.SfNumberValid = true
+	addAttrs.SfNumber = uint32(sfnum)
+	addAttrs.PfNumber = 0
+	port, err2 := DevLinkPortAdd(socket, dev.BusName, dev.DeviceName, 7, addAttrs)
+	if err2 != nil {
+		t.Fatal(err2)
+		return
+	}
+	t.Log(*port)
+	if port.Fn != nil {
+		t.Log("function attributes = ", *port.Fn)
+	}
+	macAttr := DevlinkPortFnSetAttrs{
+		FnAttrs: DevlinkPortFn{
+			HwAddr: net.HardwareAddr{0x00, 0x11, 0x22, 0x33, 0x44, 0x55},
+		},
+		HwAddrValid: true,
+	}
+	err2 = DevlinkPortFnSet(socket, dev.BusName, dev.DeviceName, port.PortIndex, macAttr)
+	if err2 != nil {
+		t.Log("function mac set err = ", err2)
+	}
+	stateAttr.FnAttrs.State = 1
+	stateAttr.StateValid = true
+	err2 = DevlinkPortFnSet(socket, dev.BusName, dev.DeviceName, port.PortIndex, stateAttr)
+	if err2 != nil {
+		t.Log("function state set err = ", err2)
+	}
+
+	port, err3 := DevLinkGetPortByIndex(socket, dev.BusName, dev.DeviceName, port.PortIndex)
+	if err3 == nil {
+		t.Log(*port)
+		t.Log(*port.Fn)
+	}
+	err2 = DevLinkPortDel(socket, dev.BusName, dev.DeviceName, port.PortIndex)
+	if err2 != nil {
+		t.Fatal(err2)
+	}
+}
+
+var socket string
+var bus string
+var device string
+var sfnum uint
+var pfnum uint
+
+func init() {
+	flag.StringVar(&socket, "socketname", "mlxdevm", "socket name as devlink or mlxdevm")
+	flag.StringVar(&bus, "bus", "", "devlink device bus name")
+	flag.StringVar(&device, "device", "", "devlink device devicename")
+	flag.UintVar(&pfnum, "pfnum", 0, "devlink port pfnumber")
+	flag.UintVar(&sfnum, "sfnum", 0, "devlink port sfnumber")
+}
